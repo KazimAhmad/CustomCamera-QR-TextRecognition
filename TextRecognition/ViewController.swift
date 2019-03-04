e14b73abf870cd441162fea99ba8abdb688023f5
kazim <kazim.ahmad@rocketmail.com>
2019-03-04 14:40:08 +0000

    Initial Commit

---
+++ TextRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognition
+//
+//  Created by KazimAhmad on 04/03/2019.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
